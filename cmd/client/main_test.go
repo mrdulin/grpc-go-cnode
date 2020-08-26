d6134a8f83a1d90324e778966bfaf22ccd5d05a4
mrdulin <novaline.dulin@gmail.com>
2020-08-26 19:54:24 +0800

    add health check service

---
+++ cmd/client/main_test.go
@@ -0,0 +1,46 @@
+package main_test
+
+import (
+	"context"
+	"log"
+	"os"
+	"path"
+	"testing"
+
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+const (
+	serverAddress = "localhost:3000"
+)
+
+func TestHealthCheckService_Integration(t *testing.T) {
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	certFile := path.Join(dir, "../../assets/server.crt")
+	creds, err := credentials.NewClientTLSFromFile(certFile, "server.grpc.io")
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn, e := grpc.Dial(serverAddress, grpc.WithTransportCredentials(creds))
+
+	if e != nil {
+		panic(e)
+	}
+	defer conn.Close()
+
+	hcClient := healthpb.NewHealthClient(conn)
+	req := new(healthpb.HealthCheckRequest)
+	res, err := hcClient.Check(context.Background(), req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.GetStatus() != healthpb.HealthCheckResponse_SERVING {
+		t.Errorf("expect serving status, got: %+v", res.GetStatus())
+	}
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,19 +5,18 @@ import (
 	"log"
 	"net"
 
-	"google.golang.org/grpc/credentials"
-
-	"github.com/mrdulin/grpc-go-cnode/internal/utils/grpclogger"
-	"google.golang.org/grpc/grpclog"
-
-	"github.com/mrdulin/grpc-go-cnode/internal/utils/interceptors"
-
 	"github.com/mrdulin/grpc-go-cnode/configs"
 	"github.com/mrdulin/grpc-go-cnode/internal/protobufs/topic"
 	"github.com/mrdulin/grpc-go-cnode/internal/protobufs/user"
+	"github.com/mrdulin/grpc-go-cnode/internal/utils/grpclogger"
 	"github.com/mrdulin/grpc-go-cnode/internal/utils/http"
+	"github.com/mrdulin/grpc-go-cnode/internal/utils/interceptors"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 var (
@@ -48,11 +47,16 @@ func main() {
 		grpc.Creds(creds),
 		grpc.UnaryInterceptor(interceptors.NewUnaryInterceptor(logger)),
 	)
+	hs := health.NewServer()
+	hs.SetServingStatus("", healthpb.HealthCheckResponse_SERVING)
+	healthpb.RegisterHealthServer(grpcServer, hs)
+
 	httpClient := http.NewClient()
 	userServiceImpl := user.NewUserServiceImpl(httpClient, baseurl)
 	topicServiceImpl := topic.NewTopicServiceImpl(httpClient, baseurl)
 	user.RegisterUserServiceServer(grpcServer, userServiceImpl)
 	topic.RegisterTopicServiceServer(grpcServer, topicServiceImpl)
+
 	log.Printf("gRPC server is listening on port: %s\n", port)
 	log.Fatal(grpcServer.Serve(lis))
 }
